44d98611f622a955d4682ecbdd4ed8d2e77f4c27
yooseohyeon <yooseohyeon.02@gmail.com>
2026-04-04 15:10:01 +0900

    feat: PATCH /products/:id API 구현

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -51,6 +51,7 @@ export const createProduct = asyncHandler(async (req, res) => {
 export const getProduct = asyncHandler(async (req, res) => {
   const { id } = req.params;
 
+  // id 검증
   if (!/^[0-9a-fA-F]{24}$/.test(id)) {
     const err = new Error("잘못된 ID 형식입니다");
     err.status = 400;
@@ -76,4 +77,55 @@ export const getProduct = asyncHandler(async (req, res) => {
   }
 
   res.json({ success: true, data: product });
+});
+
+export const updateProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { name, description, price, tags } = req.body;
+
+  // id 검증
+  if (!/^[0-9a-fA-F]{24}$/.test(id)) {
+    const err = new Error("잘못된 ID 형식입니다");
+    err.status = 400;
+    throw err;
+  }
+
+  // 조건부 형식 체크
+  if ("name" in req.body) {
+    if (name.length < 1 || name.length > 10) {
+      const err = new Error("상품명은 1자 이상 10자 이내여야 합니다");
+      err.status = 400;
+      throw err;
+    }
+  }
+
+  if ("description" in req.body) {
+    if (description.length < 10 || description.length > 100) {
+      const err = new Error("상품 소개는 10자 이상 100자 이내여야 합니다");
+      err.status = 400;
+      throw err;
+    }
+  }
+
+  if ("price" in req.body) {
+    if (typeof price !== "number" || price < 1) {
+      const err = new Error("판매가격은 1 이상의 숫자여야 합니다");
+      err.status = 400;
+      throw err;
+    }
+  }
+
+  if ("tags" in req.body) {
+    if (tags.some((tag) => tag.length > 5)) {
+      const err = new Error("태그는 5글자 이내여야 합니다");
+      err.status = 400;
+      throw err;
+    }
+  }
+
+  const updated = await prisma.product.update({
+    where: { id },
+    data: req.body,
+  });
+  res.json({ success: true, data: updated });
 });
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post("/", productController.createProduct);
 router.get("/:id", productController.getProduct);
+router.patch("/:id", productController.updateProduct);
 
 export default router;
