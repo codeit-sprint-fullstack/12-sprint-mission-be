0f34d656980b72930a5b2edd9868446149ed01b3
올챙이 <126138033+sojeong0302@users.noreply.github.com>
2026-04-06 11:30:43 +0900

    feat: MongoDB 연결

--- src/app.js
+++ src/app.js
@@ -2,14 +2,10 @@ import express from "express";
 
 // Express 앱 생성
 const app = express();
-const PORT = 8080;
 
 // 기본 라우트
 app.get("/", (req, res) => {
   res.send("Hello Express!");
 });
 
-// 서버 시작
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+export default app;
