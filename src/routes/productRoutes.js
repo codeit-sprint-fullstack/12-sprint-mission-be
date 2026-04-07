756ddfc1512d271e3bfdc6668862803821a8ceb1
yooseohyeon <yooseohyeon.02@gmail.com>
2026-04-07 11:29:36 +0900

    feat: 숫자형 ID 검증 validateId 미들웨어 추가

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import setResource from "../middleware/setResource.js";
+import validateId from "../middleware/validateId.js";
 import * as productController from "../controllers/productController.js";
 
 const router = express.Router();
@@ -8,8 +9,8 @@ router.use(setResource("상품"));
 
 router.get("/", productController.getProducts);
 router.post("/", productController.createProduct);
-router.get("/:id", productController.getProduct);
-router.patch("/:id", productController.updateProduct);
-router.delete("/:id", productController.deleteProduct);
+router.get("/:id", validateId, productController.getProduct);
+router.patch("/:id", validateId, productController.updateProduct);
+router.delete("/:id", validateId, productController.deleteProduct);
 
 export default router;
