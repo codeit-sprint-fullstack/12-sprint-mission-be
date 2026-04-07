aa6c1402af38b9a5b68b4989201a0709c71713fd
yooseohyeon <yooseohyeon.02@gmail.com>
2026-04-07 18:18:08 +0900

    chore: 시드 데이터 초기화 로직 구현 및 상품/댓글 데이터 추가

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,22 +1,64 @@
 import prisma from "../src/lib/prisma.js";
 
-async function main() {
-  await prisma.article.createMany({
+async function reset() {
+  await prisma.comment.deleteMany();
+  await prisma.article.deleteMany();
+  await prisma.product.deleteMany();
+}
+
+async function seedArticles() {
+  await prisma.article.create({
+    data: {
+      title: "첫 번째 글",
+      content: "내용입니다",
+      comments: {
+        create: [{ content: "댓글1" }, { content: "댓글2" }],
+      },
+    },
+  });
+
+  await prisma.article.create({
+    data: {
+      title: "두 번째 글",
+      content: "내용입니다",
+      comments: {
+        create: [{ content: "댓글" }],
+      },
+    },
+  });
+}
+
+async function seedProducts() {
+  await prisma.product.createMany({
     data: [
       {
-        title: "첫 번째 글",
-        content: "내용입니다",
+        name: "에어팟 프로",
+        description: "2세대라 노이즈 캔슬링이 휼륭합니다",
+        price: 329000,
+        tags: ["전자제품", "에어팟", "애플"],
+        favoriteCount: 15,
       },
       {
-        title: "두 번째 글",
-        content: "내용입니다",
+        name: "로지텍 마우스",
+        description: "블루투스 마우스",
+        price: 139000,
+        tags: ["IT", "마우스"],
+        favoriteCount: 8,
       },
       {
-        title: "세 번째 글",
-        content: "내용입니다",
+        name: "스탠리 텀블러",
+        description: "보냉 잘 됩니다",
+        price: 25000,
+        tags: ["스탠리", "친환경"],
       },
     ],
   });
+}
+
+async function main() {
+  await reset();
+  await seedArticles();
+  await seedProducts();
 }
 
 main()
