e1af9162c57f37f67142f16c1b19a607beff79a3
yooseohyeon <yooseohyeon.02@gmail.com>
2026-07-07 18:00:12 +0900

    refactor: favorite 도메인 TS로 마이그레이션

--- src/features/favorite/favorite.controller.ts
+++ src/features/favorite/favorite.controller.ts
@@ -5,11 +5,12 @@ export const addFavorite = asyncHandler(async (req, res) => {
   const { id } = req.params;
 
   const isArticle = req.baseUrl.includes("articles");
+  const targetId = Number(id);
 
   const favorite = await favoriteService.addFavorite({
-    articleId: isArticle ? Number(id) : null,
-    productId: !isArticle ? Number(id) : null,
-    userId: req.user.id,
+    articleId: isArticle ? targetId : null,
+    productId: !isArticle ? targetId : null,
+    userId: req.user!.id,
   });
 
   res.status(201).json({ data: favorite });
@@ -19,11 +20,12 @@ export const removeFavorite = asyncHandler(async (req, res) => {
   const { id } = req.params;
 
   const isArticle = req.baseUrl.includes("articles");
+  const targetId = Number(id);
 
   await favoriteService.removeFavorite({
-    articleId: isArticle ? Number(id) : null,
-    productId: !isArticle ? Number(id) : null,
-    userId: req.user.id,
+    articleId: isArticle ? targetId : null,
+    productId: !isArticle ? targetId : null,
+    userId: req.user!.id,
   });
 
   res.status(204).send();
